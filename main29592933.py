d97f391fb00460e48551975cd1467915e0ad0d47
Dimitrios Tsaligopoulos <dtsaligo@csd.auth.gr>
2021-06-16 20:13:08 +0300

    Final main, refractored

--- main29592933.py
+++ main29592933.py
@@ -1,4 +1,5 @@
-from game import Game
+from classes import Game
+
 
 class Main(object):
     def guidelines(self):
@@ -14,38 +15,56 @@ class Main(object):
             Computer: Κληρωνομεί τα πεδία και κάνει implement την δική της εκδοχή της play με τον αλγόριθμο του σεναρίου.
             Game: Βασική κλάση υπεύθυνη για την λειτουργία του παιχνιδιού. Παρέχει μεθόδους για το τρεξιμο, τον έλεγχο
             και τα αποτελέσματα μετά το παιχνίδι
-            Δομές που χρησιμοποιήθηκαν : Για τα γράμματα,το πλήθος τους και την αξία τους χρησιμοποιήθηκε dictionary όπου
-            η μέση πολυπλοκότητα προσπέλασης είναι Ο[1].
+            Δομές που χρησιμοποιήθηκαν : Για το λεξικό, τα γράμματα,το πλήθος τους και την αξία τους χρησιμοποιήθηκε
+            dictionary, όπου η μέση πολυπλοκότητα προσπέλασης είναι Ο[1].
             -
             Απαραίτητα αρχεία για τη σωστή λειτουργία είναι:
-            main-2959.py
+            main29592933.py
             classes.py
             greek7.txt
             scores.json(έχει ήδη κάποια entries μετά απο testing, τα νέα σκορ θα προστεθούν στο ήδη υπάρχον αρχείο)
         '''
 
         pass
+
     def show_menu(self):
         print('***** SCRABBLE ***** \n1:Score\n2:Options\n3:Game\nq:Exit')
+
     def player_input(self):
         x = input()
+        gamemode = 'min'
         if x == 'q':
             exit()
         elif x == str('1'):
             Game.retrieve_scores()
             Main.show_menu(self)
             Main.player_input(self)
+        elif x == str('2'):
+            inp = input("Επέλεξε τρόπο παιχνιδιού υπολογιστή!\n1.Min(Default)\n2.Max\n3.Smart\n4.Smart-Fail\n")
+            if inp == str('1'):
+                gamemode = 'min'
+            elif inp == str('2'):
+                gamemode = 'max'
+            elif inp == str('3'):
+                gamemode = 'smart'
+            elif inp == str('4'):
+                gamemode = 'smart-fail'
+            else:
+                print("Λάθος input! Το default είναι το min")
+            print("Παιχνίδι σε {} mode".format(gamemode))
+            paixnidi = Game('human', gamemode)
+            paixnidi.run()
+
         elif x == str('3'):
-            paixnidi = Game('human', 'min')
+            paixnidi = Game('human', gamemode)
             paixnidi.run()
         else:
             print('Wrong Input! Try again!')
             Main.show_menu(self)
             Main.player_input(self)
 
+
 if __name__ == '__main__':
     main = Main()
     main.show_menu()
     main.player_input()
-
-
